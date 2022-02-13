d30b6b92f8d48b41d82b67bb9f5103af7732643d
Lior <85250652+underscorelior@users.noreply.github.com>
2022-02-12 21:12:11 -0800

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -7,5 +7,4 @@ class Moderation(commands.Cog):
 	@commands.command(aliases=["strike"])
 	@commands.has_any_role("Moderator")
 	async def warn(self,ctx,id: discord.Member,*,reason): embed = discord.Embed(title="Strike",color=0xff0000); embed.set_author(name = "The Bread Pirate",icon_url="https://cdn.discordapp.com/attachments/808448077614415882/837336840272609290/Better_bread_server.gif"); embed.add_field(name="You have been warned for:",value=reason); embed.set_footer(text="If you think this was an error, reply to this message!"); await id.send(embed=embed); await ctx.send(content=f"{id.name} was warned:",embed=embed)
-def setup(bot):
-		bot.add_cog(Moderation(bot))
+def setup(bot): bot.add_cog(Moderation(bot))
