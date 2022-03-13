c359532e2e6025cc6b04d5e4ea0fa626377f6616
underscorelior <newlioryt@gmail.com>
2022-03-12 22:49:44 -0800

    More readable

---
+++ cogs/countries.py
@@ -0,0 +1,24 @@
+from discord.ext import tasks, commands
+
+class Countries(commands.Cog):
+    def __init__(self, bot):
+        self.index = 0
+        self.bot = bot
+        self.printer.start()
+
+    def cog_unload(self):
+        self.printer.cancel()
+
+    @tasks.loop(seconds=5.0)
+    async def printer(self):
+        print(self.index)
+        self.index += 1
+
+    @printer.before_loop
+    async def before_printer(self):
+        print('waiting...')
+        await self.bot.wait_until_ready()
+
+
+def setup(bot):
+    bot.add_cog(Countries(bot))
