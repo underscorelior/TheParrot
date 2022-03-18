ae9ddd397ca6156bb18ec1a92d46c1e7e59ed0b9
underscorelior <newlioryt@gmail.com>
2022-03-17 17:29:36 -0700

    Add LeaderBoard

--- cogs/countries.py
+++ cogs/countries.py
@@ -5,18 +5,35 @@ import aiohttp
 import discord
 from datetime import datetime
 import asyncio
-import time
-	
+import json
+global amounts
+try:
+	print("Successfully loaded amounts.json")
+	with open('lb.json') as f:
+		amounts = json.load(f)
+except FileNotFoundError:
+	print("Could not load amounts.json")
 class Countries(commands.Cog):
 	def __init__(self, bot):
 		self.index = 0
 		self.bot = bot
 		self.countries.start()
+	
+	@commands.command()
+	async def leaderboard(self,ctx):
+		with open('lb.json', 'r') as f:
+			data = json.load(f)
+		top_users = {k: v for k, v in sorted(data.items(), key=lambda item: item[1], reverse=True)}
+		names = ''
+		for postion, user in enumerate(top_users):
+			names += f'{postion+1} - <@!{user}> with {top_users[user]}\n'
+		embed = discord.Embed(title="Leaderboard")
+		embed.add_field(name="Names", value=names, inline=False)
+		await ctx.send(embed=embed)
 
 	def cog_unload(self):
 		self.countries.cancel()
-	#Add history questions
-    # add score
+	
 	@tasks.loop(seconds=15)
 	async def countries(self):
 		guild=self.bot.get_guild(722086066596741144)
@@ -52,17 +69,30 @@ class Countries(commands.Cog):
 					await em.edit(embed=discord.Embed(title="No one answered correctly!",description=f'What is the capital of `{quizans["name"]}:` \nReal Answer: `{quizans["capital"]}`',color=0xfa8e23, timestamp = datetime.utcnow()))
 				else:
 					await em.edit(embed=discord.Embed(title="No one answered correctly!",description=f'Which country does this flag belong to?: \nReal Answer: `{quizans["name"]}`',color=0xfa8e23, timestamp = datetime.utcnow()).set_thumbnail(url=quizans["flags"]))
+			
+			
 			else: 
-				if t == "capital": await em.edit(embed=discord.Embed(title=f'{message.author} answered correctly!',description=f'What is the capital of `{quizans["name"]}`: \nAnswer: `{quizans["capital"]}`', color=0x3cb556, timestamp = datetime.utcnow()).set_author(name=message.author,icon_url=message.author.avatar_url))
-				else: await em.edit(embed=discord.Embed(title=f'{message.author} answered correctly!',description=f'Which country does this flag belong to? \nAnswer: `{quizans["name"]}`', color=0x3cb556, timestamp = datetime.utcnow()).set_thumbnail(url=quizans["flags"]).set_author(name=message.author,icon_url=message.author.avatar_url))
+				with open('lb.json', 'r+') as f:
+					data = json.load(f)
+				if str(message.author.id) not in amounts.keys():
+					amounts[str(message.author.id)] = 1
+					_save()
+				else:
+					amounts[str(message.author.id)] += 1
+					_save()
 				await message.add_reaction("✅")
+				if t == "capital": await em.edit(embed=discord.Embed(title=f'{message.author} answered correctly!',description=f'What is the capital of `{quizans["name"]}`: \nAnswer: `{quizans["capital"]}`', color=0x3cb556, timestamp = datetime.utcnow()).set_author(name=message.author,icon_url=message.author.avatar_url).set_footer(text=f"They have a total of {data[str(message.author.id)]+1} point(s)!"))
+				else: await em.edit(embed=discord.Embed(title=f'{message.author} answered correctly!',description=f'Which country does this flag belong to? \nAnswer: `{quizans["name"]}`', color=0x3cb556, timestamp = datetime.utcnow()).set_thumbnail(url=quizans["flags"]).set_author(name=message.author,icon_url=message.author.avatar_url).set_footer(text=f"They have a total of {data[str(message.author.id)]+1} point(s)!"))
 			finally: 
 				print("Done")
+				
 	@countries.before_loop
 	async def before_countries(self):
 		print('Starting')
 		await self.bot.wait_until_ready()
 
-
+def _save():
+    with open('lb.json', 'w+') as f:
+        json.dump(amounts, f)
 def setup(bot):
 	bot.add_cog(Countries(bot))
